1103007f3922cdc9149cebc1d16e46cdb9ae028e
AlejandroM-Daw <alejandrompruebas@gmail.com>
2021-02-05 17:45:28 +0100

    Request Body

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class UsuarioController {
 	private IUsuarioDAO usuarioDAO;
 	
 	@PostMapping("/save")
-	public void save(Usuario usuario) {
+	public void save(@RequestBody Usuario usuario) {
 		usuarioDAO.save(usuario);
 	}
 	
