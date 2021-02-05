9bf3ba581f565003dc7054524895209739ee27a8
AlejandroM-Daw <alejandrompruebas@gmail.com>
2021-02-05 17:17:13 +0100

    Tercer commit

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.example.demo.models.entities.Usuario;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin({"*"})
 public class UsuarioController {
 	
 	@Autowired
