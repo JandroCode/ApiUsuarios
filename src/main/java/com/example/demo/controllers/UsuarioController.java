06766ad6f42c5e314e84134f284e8d5089c11d39
AlejandroM-Daw <alejandrompruebas@gmail.com>
2021-02-11 18:18:04 +0100

    Crud completo

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -2,10 +2,15 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +34,29 @@ public class UsuarioController {
 	@GetMapping("/list")
 	public List<Usuario> list(){
 		return usuarioDAO.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Usuario getUser(@PathVariable(name = "id")Long id) {
+		return usuarioDAO.findById(id).orElse(null);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteUser(@PathVariable Long id){
+		usuarioDAO.deleteById(id);
+	}
+	
+	@PutMapping("/update/{id}")
+	public Usuario updateUser(Usuario usuario, @PathVariable Long id) {
+		Usuario nuevoUsuario = usuarioDAO.findById(id).orElse(null);
+		nuevoUsuario.setNombre(usuario.getNombre());
+		nuevoUsuario.setApellidos(usuario.getApellidos());
+		nuevoUsuario.setEmail(usuario.getEmail());
+		nuevoUsuario.setTelefono(usuario.getTelefono());
+		
+		return nuevoUsuario;
+		
+	
 	}
 	
 	
